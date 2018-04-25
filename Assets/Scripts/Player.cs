0932b172525abf3f14fb87d6202b55a7df819d97
root <root@DESKTOP-18T5OIQ.localdomain>
2018-04-25 11:08:01 +0300

    added checkpoints

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-    public float moveSpeed = 5f;        // скорость игрока.
+    public float moveSpeed = 5f;                    // скорость игрока.
 
-    private bool canMove = true;                // можем ли мы двигаться?
+    private bool canMove = true;                    // можем ли мы двигаться?
+    private Vector3 spawnPoint = Vector3.zero;      // сюда сохраняем последний чекпоинт.
+    private Rigidbody rb;                           // rigidbody.
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        spawnPoint = transform.position;
+    }
 
     private void Update()
     {
@@ -26,6 +34,28 @@ public class Player : MonoBehaviour {
         {
             canMove = false;
         }
+
+        // чекпоинт.
+        if (col.gameObject.tag == "CheckPoint")
+        {
+            spawnPoint = transform.position;
+        }
+
+        // упали.
+        if (col.gameObject.tag == "Die")
+        {
+            RespawnPlayer();
+        }
+
+    }
+
+    // респавн игрока.
+    private void RespawnPlayer()
+    {
+        canMove = true;
+        rb.velocity = Vector3.zero;
+        transform.rotation = new Quaternion(0f, 0f, 0f, 0f);
+        transform.position = spawnPoint;
     }
 
 }
